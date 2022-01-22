4eb3199d9a70ebdd290f665036b128e6e20d07a9
Aleck Bowen Sun <aleck.bowen.sun@gmail.com>
2022-01-22 10:42:16 -0500

    Fix parameter error

--- server/app.py
+++ server/app.py
@@ -8,7 +8,7 @@ from google.cloud import texttospeech_v1
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "serviceAccount.json"
 client = texttospeech_v1.TextToSpeechClient()
 gender = texttospeech_v1.SsmlVoiceGender.NEUTRAL
-voice = texttospeech_v1.VoiceSelectionParams(Language_code="en-US", ssml_gender=gender)
+voice = texttospeech_v1.VoiceSelectionParams(language_code="en-US", ssml_gender=gender)
 audio_config = texttospeech_v1.AudioConfig(audio_encoding=texttospeech_v1.AudioEncoding.MP3)
 
 # Create server
