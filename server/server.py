cc9a301058e9a4e568a959c31713e2c1b19b9c93
Aleck Bowen Sun <aleck.bowen.sun@gmail.com>
2022-01-22 00:22:10 -0500

    "Update CORS"

--- server/server.py
+++ server/server.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 
 @app.route("/")
 def main():
