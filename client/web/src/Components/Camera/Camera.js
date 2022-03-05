e9b2d7dcdf7bd13eb34f8616a51b4f773d9fdb40
Aleck Bowen Sun <aleck.bowen.sun@gmail.com>
2022-03-05 16:45:22 -0500

    Add camera switching feature

--- client/web/src/Components/Camera/Camera.js
+++ client/web/src/Components/Camera/Camera.js
@@ -1,23 +1,31 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import rearPrompt from '../../Images/rearmode.mp3';
+import frontPrompt from '../../Images/frontmode.mp3';
 import './Camera.css';
 
+let clicks = 0;
 const Camera = () => {
     const videoRef = useRef(null);
     const photoRef = useRef(null);
     const audioRef = useRef(null);
+    const [facingMode, setFacingMode] = useState("user");
 
     // Get camera feed and play in video element
     const getVideo = () => {
         navigator.mediaDevices
             .getUserMedia({
                 video: {
-                    facingMode: "user",
+                    facingMode: facingMode,
                 }
             })
             .then(stream => {
                 let video = videoRef.current;
                 video.srcObject = stream;
                 video.play();
+
+                // Audio prompt for camera facingmode
+                const prompt = facingMode == "user" ? frontPrompt : rearPrompt;
+                playAudio(prompt);
             })
             .catch(err => {
                 console.log(err);
@@ -46,30 +54,41 @@ const Camera = () => {
         audio.play();
     };
 
-    // Takes a snapshot of video and classifies
     const getPhoto = () => {
-        let video = videoRef.current;
-        let photo = photoRef.current;
-        photo.width = video.videoWidth;
-        photo.height = video.videoHeight;
+        // Switch camera angle
+        if (clicks === 2) {
+            const newMode = facingMode == "user" ? "environment" : "user";
+            setFacingMode(newMode);
+        } else if (clicks == 1) {
+            // Takes a snapshot of video and classifies
+            let video = videoRef.current;
+            let photo = photoRef.current;
+            photo.width = video.videoWidth;
+            photo.height = video.videoHeight;
 
-        try {
-            photo.getContext('2d').drawImage(video, 0, 0, photo.width, photo.height);
-            const dataURL = photo.toDataURL("image/png").split(",")[1];
+            try {
+                photo.getContext('2d').drawImage(video, 0, 0, photo.width, photo.height);
+                const dataURL = photo.toDataURL("image/png").split(",")[1];
 
-            // Send dataURL to backend and get sound
-            sendImage(dataURL);
-        } catch (err) {
-            console.log(err);
-        }
+                // Send dataURL to backend and get sound
+                sendImage(dataURL);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        clicks = 0;
     };
 
     useEffect(() => {
         getVideo();
-    }, [videoRef]);
+    }, [facingMode]);
 
     return(
-        <div className="camera-wrapper" onClick={getPhoto}>
+        <div className="camera-wrapper" onClick={() => {
+                clicks += 1;
+                setTimeout(getPhoto, 500);
+            }
+        }>
             <video className="camera" ref={videoRef}></video>
             <canvas className="canvas" ref={photoRef}></canvas>
             <audio className="audio" ref={audioRef}></audio>
